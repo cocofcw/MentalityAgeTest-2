7318d015d5cae27493b71e8e26b583760cc6a885
fuchaowen <fcw@fuchaowendeMacBook-Pro.local>
2014-08-27 16:40:39 +0800

    Initial Commit

---
+++ MentalityAgeTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MentalityAgeTest
+//
+//  Created by fuchaowen on 14-8-27.
+//  Copyright (c) 2014年 com.fuchaowen.www. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
